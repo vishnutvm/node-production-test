75e51e582ee152ad4923a801191e3dddc88e636d
vishnu M <vishnum@stuidumtech.in>
2023-09-10 20:19:17 +0530

    chore:Redirecting call to my hosted ip

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const httpProxy = require('http-proxy');
 
-app.get('/', (req, res) => {
-    res.send('Server Working edited !');
+const app = express();
+const proxy = httpProxy.createProxyServer();
+
+const awsServerUrl = 'http://13.230.210.94';
+
+app.all('/*', (req, res) => {
+    proxy.web(req, res, { target: awsServerUrl });
 });
 
+const port = 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Reverse proxy server is running on port ${port}`);
 });
